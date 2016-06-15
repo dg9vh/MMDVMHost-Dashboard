ab37f73a29b518b16a916378099714f44b1444d2
ayasystems <angel@ayasystems.com>
2016-06-15 15:21:14 +0200

    Create halt.php

---
+++ scripts/halt.php
@@ -0,0 +1,65 @@
+<?php
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$start = $time;
+
+// do not touch this includes!!! Never ever!!!
+include "../config/config.php";
+include "../include/tools.php";
+include "../include/functions.php";
+?>
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <!-- Das neueste kompilierte und minimierte CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!-- Optionales Theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+    <!-- Das neueste kompilierte und minimierte JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <title><?php echo getCallsign($mmdvmconfigs) ?> - MMDVM-Dashboard by DG9VH</title>
+
+  </head>
+  <body>
+  <div class="page-header">
+  <h1><small>MMDVM-Dashboard by DG9VH for <?php
+  if (getConfigItem("General", "Duplex", $mmdvmconfigs) == "1") {
+  	echo "Repeater";
+  } else {
+  	echo "Hotspot";  
+  }
+  ?>:</small>  <?php echo getCallsign($mmdvmconfigs) ?></h1>
+  <h4>MMDVMHost by G4KLX Version: <?php echo getMMDVMHostVersion() ?></h4>
+  <button onclick="window.location.href='../index.php'"  type="button" class="btn btn-default navbar-btn"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home</button>
+
+  </script>
+</div>
+<?php
+checkSetup();
+include "../include/sysinfo.php";
+   exec(HALTSYS. ' > /dev/null 2>&1 &');
+?>
+<div class="alert alert-info" role="alert">Exceuteing  <b><?php echo HALTSYS ?></b><br>Halt in progress...bye</div>	
+ 
+	<div class="panel panel-info">
+
+<?php
+
+$datum = date("d-m-Y");
+$uhrzeit = date("H:i:s");
+echo "Last Update $datum, $uhrzeit";
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$finish = $time;
+$total_time = round(($finish - $start), 4);
+echo '<!--Page generated in '.$total_time.' seconds.-->';	
+?> | get your own at: <a href="https://github.com/dg9vh/MMDVMHost-Dashboard">https://github.com/dg9vh/MMDVMHost-Dashboard</a>
+	</div>
+  </body>
+</html>
