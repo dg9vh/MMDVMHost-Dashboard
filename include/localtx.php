6e1d8e1dc23af5802a5bfb65d2c46bf0c972fcb0
EA4GKQ <angel@ayasystems.com>
2016-06-24 21:37:52 +0200

    Update localtx.php

--- include/localtx.php
+++ include/localtx.php
@@ -1,15 +1,16 @@
 <?php
 
-$localTXList = getHeardList($reverseLogLines);
+$localTXList = getHeardList($reverseLogLinesMMDVM);
 //array_multisort($localTXList,SORT_DESC);
 
 ?>
   <div class="panel panel-default">
   <!-- Standard-Panel-Inhalt -->
-  <div class="panel-heading">Today's last 10 local transmissions.</div>
+  <div class="panel-heading">Today's last 20 local transmissions.</div>
   <!-- Tabelle -->
 <div class="table-responsive">  
-  <table class="table table-condensed">
+  <table id="localTx" class="table table-condensed">
+   <thead>
     <tr>
       <th>Time (UTC)</th>
       <th>Mode</th>
@@ -21,30 +22,32 @@ $localTXList = getHeardList($reverseLogLines);
       <th>Loss</th>
       <th>BER</th>
     </tr>
+   </thead>
+   <tbody>	
 <?php
 $counter = 0;
 for ($i = 0; $i < count($localTXList); $i++) {
 		$listElem = $localTXList[$i];		
 		if ($listElem[5] == "RF" && ($listElem[1]=="D-Star" || startsWith($listElem[1], "DMR") || $listElem[1]=="YSF")) {
 			echo"<tr>";
-			echo"<td>$listElem[0]</td>";
-			echo"<td>$listElem[1]</td>";
-			echo"<td>$listElem[2]</td>";
-			echo"<td>$listElem[3]</td>";
-			echo"<td>$listElem[4]</td>";
-			echo"<td>$listElem[5]</td>";
+			echo"<td nowrap>$listElem[0]</td>";
+			echo"<td nowrap>$listElem[1]</td>";
+			echo"<td nowrap>$listElem[2]</td>";
+			echo"<td nowrap>$listElem[3]</td>";
+			echo"<td nowrap>$listElem[4]</td>";
+			echo"<td nowrap>$listElem[5]</td>";
 			if ($listElem[6] == null) {
-				echo'<td colspan="3">in TX</td>';
+				echo'<td  nowrap colspan="3">in TX</td>';
 			} else if ($listElem[6] == "SMS") {
-				echo'<td colspan="3">sending or receiving SMS</td>';
+				echo'<td nowrap colspan="3">sending or receiving SMS</td>';
 			} else {
-				echo"<td>$listElem[6]</td>";
-				echo"<td>$listElem[7]</td>";
-				echo"<td>$listElem[8]</td>";
+				echo"<td nowrap>$listElem[6]</td>";
+				echo"<td nowrap>$listElem[7]</td>";
+				echo"<td nowrap>$listElem[8]</td>";
 			}
 			echo"</tr>\n";
 			$counter++;
-			if ($counter == 10) {
+			if ($counter == 20) {
 				break;
 			}
 		}
@@ -52,6 +55,16 @@ for ($i = 0; $i < count($localTXList); $i++) {
 
 ?>
    
+  </tbody>
   </table>
  </div>
 </div>
+<script>
+$(document).ready(function(){
+  
+  $('#localTx').dataTable( {
+    "aaSorting": [[0,'desc']]
+  } );
+	
+});
+</script>
