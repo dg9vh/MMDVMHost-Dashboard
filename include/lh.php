bac47ad98ac8b31dbd18a560ecc2aa2eb768355c
EA4GKQ <angel@ayasystems.com>
2016-06-24 21:24:34 +0200

    Update lh.php

--- include/lh.php
+++ include/lh.php
@@ -24,24 +24,24 @@
 for ($i = 0; ($i < LHLINES) AND ($i < count($lastHeard)); $i++) {
 		$listElem = $lastHeard[$i];
 		echo"<tr>";
-		echo"<td>$listElem[0]</td>";
-		echo"<td>$listElem[1]</td>";
-		echo"<td>$listElem[2]</td>";
-		echo"<td>$listElem[3]</td>";
-		echo"<td>$listElem[4]</td>";
+		echo"<td nowrap>$listElem[0]</td>";
+		echo"<td nowrap>$listElem[1]</td>";
+		echo"<td nowrap>$listElem[2]</td>";
+		echo"<td nowrap>$listElem[3]</td>";
+		echo"<td nowrap>$listElem[4]</td>";
 		if ($listElem[5] == "RF"){
-			echo "<td><span class=\"label label-success\">RF</span></td>";
+			echo "<td nowrap><span class=\"label label-success\">RF</span></td>";
 		}else{
-			echo"<td>$listElem[5]</td>";
+			echo"<td nowrap>$listElem[5]</td>";
 		}
 		if ($listElem[6] == null) {
-				echo'<td colspan="3">transmitting</td>';
+				echo'<td  nowrap colspan="3">transmitting</td>';
 			} else if ($listElem[6] == "SMS") {
-				echo'<td colspan="3">sending or receiving SMS</td>';
+				echo'<td  nowrap colspan="3">sending or receiving SMS</td>';
 			} else {
-			echo"<td>$listElem[6]</td>";
-			echo"<td>$listElem[7]</td>";
-			echo"<td>$listElem[8]</td>";
+			echo"<td nowrap>$listElem[6]</td>";
+			echo"<td nowrap>$listElem[7]</td>";
+			echo"<td nowrap>$listElem[8]</td>";
 		}
 		echo"</tr>\n";
 	}
