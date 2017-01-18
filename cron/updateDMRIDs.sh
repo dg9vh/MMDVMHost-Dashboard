5a8b1a7730afc8c14e610696a23f90ca7471ecb8
phl0 <florian@florian-wolters.de>
2017-01-18 15:20:21 +0100

    Add license information

--- cron/updateDMRIDs.sh
+++ cron/updateDMRIDs.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
+#
+###############################################################################
+#
+# updateDMRIDs.sh
+#
+# Copyright (C) 2017 by Florian Wolters DF2ET
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+###############################################################################
+#
+# 18/1/17 - This script is a "derivative work" of GPL version 2 code Copyright
+# by Tony Corbett G0WFV and is reproduced in this CC0 Universal licensed
+# project with the original authors' express consent.
+#
+###############################################################################
+
 
 # Full path to DMR ID file
 DMRIDFILE=/var/www/html/DMRIds.dat
