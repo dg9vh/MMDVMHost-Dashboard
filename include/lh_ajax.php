1731c8fb0eed4b749c564792e0503cc8351d68d6
dg9vh <dg9vh@darc.de>
2017-04-07 13:17:37 +0000

    only shows cached if name resolution is switched on

--- include/lh_ajax.php
+++ include/lh_ajax.php
@@ -1,12 +1,19 @@
 <?php
 $totalLH = count($lastHeard);
+if (defined("ENABLEXTDLOOKUP")) {
 $TMP_CALL_NAME = "/tmp/Callsign_Name.txt";
 exec("wc -l ".$TMP_CALL_NAME." | cut -f1 -d' '", $output);
 exec("wc -l ".DMRIDDATPATH." | cut -f1 -d' '", $output2);
+}
 ?>
   <div class="panel panel-default">
   <!-- Standard-Panel-Inhalt -->
-  <div class="panel-heading"><?php echo _("Last Heard List of today's")." ".$totalLH." "._("callsigns.")." "._("Cached")." (".$output[0]."/".$output2[0].")"; ?><span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-up"></i></span></div>
+  <div class="panel-heading"><?php 
+  echo _("Last Heard List of today's")." ".$totalLH." "._("callsigns.")." ";
+  if (defined("ENABLEXTDLOOKUP")) {
+    echo _("Cached")." (".$output[0]."/".$output2[0].")";
+  }
+  ?><span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-up"></i></span></div>
   <div class="panel-body">
   <!-- Tabelle -->
   <div class="table-responsive">
