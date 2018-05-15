fb9c6c8a5d208c08103171bd83c53b2d59b4c754
phl0 <florian@florian-wolters.de>
2018-05-15 21:46:37 +0200

    Add "www." to prevent SSL certificate errors

--- database/dbupdate.sh
+++ database/dbupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Downloading DMR-IDs from MARC-database
-curl 'https://radioid.net/static/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s;%s;%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > dmrids.dat
+echo Downloading DMR-IDs from RadioID database
+curl 'https://www.radioid.net/static/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s;%s;%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > dmrids.dat
 
 echo Removing IDs from local database
 echo -e 'delete from callsign where 1;' | sqlite3 callsigns.db
