4ba4ba20968ada761e2b51fbe012be730812300a
Kim Huebel <dg9vh@darc.de>
2018-06-04 11:54:16 +0000

    New URL for database Update to get EU-Calls again.

--- database/dbupdate.sh
+++ database/dbupdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo Downloading DMR-IDs from RadioID database
-curl 'https://www.ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s;%s;%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > dmrids.dat
+#curl 'https://www.ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s;%s;%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > dmrids.dat
+curl 'https://ham-digital.org/status/dmrid.dat' 2>/dev/null > dmrids.dat
 
 echo Removing IDs from local database
 echo -e 'delete from callsign where 1;' | sqlite3 callsigns.db
