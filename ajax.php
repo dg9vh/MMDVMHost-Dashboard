34210147a758ce9e5715af829b9249ddeb2149ef
Kim Hübel <dg9vh@darc.de>
2020-10-30 13:47:34 +0000

    Fixed local-tx-list after removing D-Star...

--- ajax.php
+++ ajax.php
@@ -30,7 +30,6 @@ $logLinesMMDVM = getMMDVMLog();
 $reverseLogLinesMMDVM = $logLinesMMDVM;
 rsort($reverseLogLinesMMDVM);
 
-
 if (defined("ENABLEDAPNETGATEWAY")) {
    $logLinesDAPNETGateway = getDAPNETGatewayLog();
    showLapTime("getDAPNETGatewayLog");
@@ -112,7 +111,8 @@ if ($_GET['section'] == "localTx") {
       }
       if (defined("ENABLEXTDLOOKUP")) {
          //$listElem[11] ="";
-         if ($listElem[6] == "RF" && ($listElem[1]=="D-Star" || startsWith($listElem[1], "DMR") || $listElem[1]=="YSF" || $listElem[1]=="P25" || $listElem[1]=="NXDN")) {
+         if (getEnabled("D-Star", $mmdvmconfigs) == 1 && $listElem[6] == "RF" && ($listElem[1]=="D-Star" || startsWith($listElem[1], "DMR") || $listElem[1]=="YSF" || $listElem[1]=="P25" || $listElem[1]=="NXDN") || \
+         getEnabled("D-Star", $mmdvmconfigs) == 0 && $listElem[5] == "RF" && ($listElem[1]=="D-Star" || startsWith($listElem[1], "DMR") || $listElem[1]=="YSF" || $listElem[1]=="P25" || $listElem[1]=="NXDN")) {
             $listElem[3] = getName($call_canon);
             if ($listElem[2] !== "??????????") {
                if (!is_numeric($listElem[2])) {
@@ -130,7 +130,8 @@ if ($_GET['section'] == "localTx") {
          }
       } else {
          //$listElem[10] ="";
-         if ($listElem[5] == "RF" && ($listElem[1]=="D-Star" || startsWith($listElem[1], "DMR") || $listElem[1]=="YSF" || $listElem[1]=="P25" || $listElem[1]=="NXDN")) {
+         if (getEnabled("D-Star", $mmdvmconfigs) == 1 && $listElem[5] == "RF" && ($listElem[1]=="D-Star" || startsWith($listElem[1], "DMR") || $listElem[1]=="YSF" || $listElem[1]=="P25" || $listElem[1]=="NXDN") || \
+         getEnabled("D-Star", $mmdvmconfigs) == 0 && $listElem[4] == "RF" && ($listElem[1]=="D-Star" || startsWith($listElem[1], "DMR") || $listElem[1]=="YSF" || $listElem[1]=="P25" || $listElem[1]=="NXDN")) {
             if ($listElem[2] !== "??????????") {
                if (!is_numeric($listElem[2])) {
                   if (defined("SHOWQRZ")) {
